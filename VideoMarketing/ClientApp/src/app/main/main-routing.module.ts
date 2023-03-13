da978057f775d186851d51e73768ddabba6dea1b
Alberto Ramirez <83984063+beto-devnet@users.noreply.github.com>
2023-03-13 14:54:12 -0600

    update to lazy loading

--- VideoMarketing/ClientApp/src/app/main/main-routing.module.ts
+++ VideoMarketing/ClientApp/src/app/main/main-routing.module.ts
@@ -1,27 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AppointmentComponent } from './appointment/appointment.component';
-import { ProfileComponent } from './profile/profile.component';
-import { EditProfileComponent } from './profile/edit-profile/edit-profile.component';
+import { MainComponent } from './main.component';
 
 const routes: Routes = [
   {
-    path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-  },
-  {
-    path: 'appointment',
-    loadChildren: () => import('./appointment/appointment.module').then(m => m.AppointmentModule)
-  },
-  {
-    path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
-  },
-  {
-    path: 'edit-profile',
-    component: EditProfileComponent
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+      },
+      {
+        path: 'appointment',
+        loadChildren: () => import('./appointment/appointment.module').then(m => m.AppointmentModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+      }
+    ]
   }
+  // ,
+  // {
+  //   path: 'edit-profile',
+  //   component: EditProfileComponent
+  // }
 ];
 
 @NgModule({
