d04b2684299a7d7f30ec68561e7ab3ec886ba641
Alberto Ramirez <83984063+beto-devnet@users.noreply.github.com>
2023-03-14 10:54:22 -0600

    updated MediatR

--- VideoMarketing/Program.cs
+++ VideoMarketing/Program.cs
@@ -4,7 +4,7 @@ using MediatR;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly));
 
 builder.Services.AddControllersWithViews();
 
