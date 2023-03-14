69aecfe6216fac435fe4ec8ac30e56be6a85fa91
Alberto Ramirez <83984063+beto-devnet@users.noreply.github.com>
2023-03-14 10:49:34 -0600

    test

--- VideoMarketing/ClientApp/src/app/security/security.module.ts
+++ VideoMarketing/ClientApp/src/app/security/security.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SecurityRoutingModule } from './security-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -11,7 +15,11 @@ import { SecurityRoutingModule } from './security-routing.module';
   ],
   imports: [
     CommonModule,
-    SecurityRoutingModule
+    SecurityRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class SecurityModule { }
--- VideoMarketing/Controllers/Features/Appointment/GetClasses.cs
+++ VideoMarketing/Controllers/Features/Appointment/GetClasses.cs
@@ -13,12 +13,14 @@ public class GetAppointments
 		// public CommandHandler(DbContext context) => _context = context;
 
 		public async Task<List<Response>> Handle(Query request, CancellationToken cancellationToken) =>
-			new List<Response>()
+			await Task.FromResult(new List<Response>()
 			{
 				new Response(1, "Felipe", "Cardio", new TimeOnly(9, 0), 45),
 				new Response(2, "Bro", "Weights", new TimeOnly(12, 0), 45),
 				new Response(3, "Rocket", "Lobby", new TimeOnly(15, 30), 45),
 				new Response(4, "Lance", "Boxing", new TimeOnly(15, 30), 45)
-			};
+			});
+
+		
 	}
 }
